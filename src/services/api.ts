b5b725ca9b31bc331adcef4c55c4368ffe095ec5
rancagp <ranca632@gmail.com>
2026-01-07 16:15:05 +0700

    fixing error service api karier

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://rfbdev.newsmaker.id',
+  baseURL: 'https://rfbdev.newsmaker.id/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
