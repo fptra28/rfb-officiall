210b5957617b3918c98d99b083b03318940e27cb
rancagp <ranca632@gmail.com>
2026-01-07 16:02:51 +0700

    update jfx,spa, karier & berita

--- src/pages/informasi/umum/[slug]/index.tsx
+++ src/pages/informasi/umum/[slug]/index.tsx
@@ -5,7 +5,6 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import PageTemplate from '@/components/templates/PageTemplate';
 import ProfilContainer from '@/components/templates/PageContainer/Container';
-import NotFound from '@/components/moleculs/NotFound';
 import DetailBerita from '@/components/organisms/DetailBerita';
 
 type Berita = {
@@ -150,7 +149,7 @@ export default function BeritaDetail() {
                     <DetailBerita
                         date={formatDate(berita.created_at)}
                         title={berita.judul}
-                        img={`https://rfbdev.newsmaker.id/img/berita/${berita.image}`}
+                        img={`http://rfb-backend.test/img/berita/${berita.image}`}
                         content={berita.isi}
                         kategori={berita.kategori || "Informasi Umum"}
                     />
